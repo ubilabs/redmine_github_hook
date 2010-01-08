f52127b2d8eff6fe065340ce431a734909b68546
Martin Kleppe <kleppe@ubilabs.net>
2010-01-08 14:39:11 +0100

    Cycle through all Git repositories instead of only a single project. This will bypass the same-identifier limitation.

--- app/controllers/github_hook_controller.rb
+++ app/controllers/github_hook_controller.rb
@@ -1,28 +1,22 @@
 require 'json'
 
 class GithubHookController < ApplicationController
-
+  
   skip_before_filter :verify_authenticity_token, :check_if_login_required
 
   def index
+    
     payload = JSON.parse(params[:payload])
     logger.debug { "Received from Github: #{payload.inspect}" }
-
-    # For now, we assume that the repository name is the same as the project identifier
-    identifier = payload['repository']['name']
-
-    project = Project.find_by_identifier(identifier)
-    raise ActiveRecord::RecordNotFound, "No project found with identifier '#{identifier}'" if project.nil?
     
-    repository = project.repository
-    raise TypeError, "Project '#{identifier}' has no repository" if repository.nil?
-    raise TypeError, "Repository for project '#{identifier}' is not a Git repository" unless repository.is_a?(Repository::Git)
-
-    # Get updates from the Github repository
-    #command = "cd '#{repository.url}' && cd .. && git pull --rebase"
-    command = "cd '#{repository.url}' && git fetch origin && git reset --soft refs/remotes/origin/master"
-    exec(command)
-
+    Repository.all.each do |repository|
+      if repository.is_a?(Repository::Git)
+        puts repository.url
+        command = "cd '#{repository.url}' && git fetch origin && git reset --soft refs/remotes/origin/master"
+        exec(command)
+      end
+    end
+    
     # Fetch the new changesets into Redmine
     repository.fetch_changesets
 
